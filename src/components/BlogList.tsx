d08516480bc06d1fb97b23f6337f62bbf2462037
Vinnydaninja <vincenguyen35@gmail.com>
2024-06-05 17:56:19 -0700

    Tested connection between client and server with DB

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,4 +1,20 @@
+// React Imports
+import { useEffect } from "react";
+
 const BlogList = () => {
+
+    useEffect(() => {
+        const fetchBlogList = async () => {
+            const response = await fetch("http://localhost:3000/api/blogs");
+            const json = await response.json();
+            // Response will return an array of objects if working
+            if (response.ok) {
+                console.log(json);
+            }
+        };
+        fetchBlogList();
+    }, []);
+
     return (
         <div className="blog-list">
             Blog List Here
